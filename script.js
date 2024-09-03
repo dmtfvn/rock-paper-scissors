2c7cf952c5942bd018e3d286686f9eca8a3cce4c
dmtfvn <dmtfvn@gmail.com>
2024-09-03 17:54:19 +0300

    Add functionality to show and reset result

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let computerMove = '';
+const gameScore = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
 
 function pickComputerMove() {
   const randomNumber = Math.random();
+
+  let computerMove = '';
 
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = 'rock';
@@ -10,55 +16,81 @@ function pickComputerMove() {
   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
     computerMove = 'scissors';
   }
+
+  return computerMove;
 }
+
+const curResult = document.querySelector('.js-cur-result');
+const finalResult = document.querySelector('.js-final-result');
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = '';
+
+  if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie';
+    } else if (computerMove === 'paper') {
+      result = 'You lose';
+    } else if (computerMove === 'scissors') {
+      result = 'You Win';
+    }
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You Win';
+    } else if (computerMove === 'paper') {
+      result = 'Tie';
+    } else if (computerMove === 'scissors') {
+      result = 'You lose';
+    }
+  } else if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose';
+    } else if (computerMove === 'paper') {
+      result = 'You Win';
+    } else if (computerMove === 'scissors') {
+      result = 'Tie';
+    }
+  }
+
+  if (result === 'You Win') {
+    gameScore.wins++;
+  } else if (result === 'You lose') {
+    gameScore.losses++;
+  } else if (result === 'Tie') {
+    gameScore.ties++;
+  }
+
+  curResult.textContent = `
+    You picked ${playerMove}. Computer picked ${computerMove}. ${result}.
+  `;
+
+  finalResult.textContent = `
+    Wins: ${gameScore.wins}, Losses: ${gameScore.losses}, Ties: ${gameScore.ties}.
+  `;
+}
+
+const reset = document.querySelector('.js-reset');
+reset.addEventListener('click', () => {
+  gameScore.wins = 0;
+  gameScore.losses = 0;
+  gameScore.ties = 0;
+});
 
 const gameButtons = document.querySelectorAll('.js-button');
 gameButtons.forEach((button) => {
   button.addEventListener('mousedown', () => {
     button.classList.add('active');
 
-    const playerMove = button.textContent;
+    const player = button.textContent;
 
-    let result = '';
-
-    if (playerMove === 'R') {
-      pickComputerMove();
-
-      if (computerMove === 'rock') {
-        result = 'Tie';
-      } else if (computerMove === 'paper') {
-        result = 'You lose';
-      } else if (computerMove === 'scissors') {
-        result = 'You Win';
-      }
-
-      console.log(`You picked rock. Computer picked ${computerMove}. ${result}.`)
-
-    } else if (playerMove === 'P') {
-      pickComputerMove();
-
-      if (computerMove === 'rock') {
-        result = 'You Win';
-      } else if (computerMove === 'paper') {
-        result = 'Tie';
-      } else if (computerMove === 'scissors') {
-        result = 'You lose';
-      }
-
-      console.log(`You picked paper. Computer picked ${computerMove}. ${result}.`)
-
-    } else if (playerMove === 'S') {
-      pickComputerMove();
-
-      if (computerMove === 'rock') {
-        result = 'You lose';
-      } else if (computerMove === 'paper') {
-        result = 'You Win';
-      } else if (computerMove === 'scissors') {
-        result = 'Tie';
-      }
-
-      console.log(`You picked scissors. Computer picked ${computerMove}. ${result}.`)
+    if (player === 'R') {
+      playGame('rock');
+    } else if (player === 'P') {
+      playGame('paper');
+    } else if (player === 'S') {
+      playGame('scissors');
     }
   });
 
